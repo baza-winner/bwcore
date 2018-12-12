f1f81c6357faa288c3f3fc225ad0ee26fc9e9594
Yury Bikuzin <yury.bikuzin@gmail.com>
2018-12-12 15:27:42 +0300

    get.sh added

---
+++ get.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+go get github.com/baza-winner/bwcore/ansi
+go get github.com/baza-winner/bwcore/bw
+go get github.com/baza-winner/bwcore/bwdebug
+go get github.com/baza-winner/bwcore/bwerr
+go get github.com/baza-winner/bwcore/bwexec
+go get github.com/baza-winner/bwcore/bwjson
+go get github.com/baza-winner/bwcore/bwmap
+go get github.com/baza-winner/bwcore/bwos
+go get github.com/baza-winner/bwcore/bwparse
+go get github.com/baza-winner/bwcore/bwrune
+go get github.com/baza-winner/bwcore/bwset
+go get github.com/baza-winner/bwcore/bwsetter
+go get github.com/baza-winner/bwcore/bwstr
+go get github.com/baza-winner/bwcore/bwtesting
+go get github.com/baza-winner/bwcore/bwtype
+go get github.com/baza-winner/bwcore/bwval
+go get github.com/mohae/deepcopy
